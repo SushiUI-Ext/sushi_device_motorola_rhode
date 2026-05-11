078828a1169ba89f3770d4de7776aa53e0d21433
bangkkuser <275179888+rebangkkuser@users.noreply.github.com>
2026-05-11 15:03:38 -0300

    Create envsetup.sh

---
+++ envsetup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (c) 2026 SushiUI Extended 
+# All rights reserved
+
+DEVICE="bangkk"
+VERSION="1.1.2"
+VENDOR="motorola"
+AB="true"
+RESOLUTION="1080x2400"
+SOC="QTI SM6375"
+ARCHITECTURES="aarch64,armeabi-v7a,armeabi"
+LATEST_SDK="35"
+FIRST_SDK="33"
+BUILDNO="V1TCS35H.88-16-4"
+BUILD_SDK="35"
+# color variables
+RED="\e[31m"
+GREEN="\e[32m"
+RESET="\e[0m"
+BLUE="\e[34m"
+# functions (commands)
+sushidevinfo() {
+  echo -e "Device ${BLUE}$DEVICE${RESET} info"
+  echo -e "SoC: $SOC"
+  echo -e "SushiUI version: $VERSION"
+  echo -e "A/B: $AB"
+  echo -e "Architectures: $ARCHITECTURES"
+}
